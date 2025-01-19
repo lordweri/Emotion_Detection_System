47011784cb0935008e4d1076b29802822ed6136b
lordweri <werimasao@gmail.com>
2025-01-19 16:41:42 -0500

    Formatted the response

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,5 +5,21 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers = headers)
-    return response.text   
 
+    formatted_response = json.loads(response.text)
+    emotion_data = formatted_response['emotionPredictions'][0]['emotion']
+    emotions = {
+        "anger": emotion_data.get('anger', 0),
+        "disgust": emotion_data.get('disgust', 0),
+        "fear": emotion_data.get('fear', 0),
+        "joy": emotion_data.get('joy', 0),
+        "sadness": emotion_data.get('sadness', 0)
+    }
+
+    dominant_emotion = max(emotions, key=emotions.get)
+    dominant_score = emotions[dominant_emotion]
+    
+    return dominant_emotion, dominant_score, emotions
+   
+
+ 
