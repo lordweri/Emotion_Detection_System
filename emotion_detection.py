d42ab9d25c4c5182ff06dc91ae05dbbdfd703ea5
lordweri <werimasao@gmail.com>
2025-01-19 16:17:41 -0500

    Task 2 done

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,6 +1,6 @@
 import requests
 import json
-def sentiment_analyzer(text_to_analyse):
+def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
